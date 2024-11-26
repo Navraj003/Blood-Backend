fe7814660ff2d486b47f7e8a9c2eeb1cb0a12457
Navraj <nkaur_be22@thapar.edu>
2024-11-26 09:55:59 +0530

    Navraj

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express from 'express';
 import cors from 'cors';
-import userRoutes from './routes/user.routes.js';
+import express from 'express';
 import adminRoutes from './routes/admin.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 const app = express();
 app.use(express.json());
@@ -9,6 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/api/user', userRoutes);
-app.use('/api/admin', adminRoutes); 
+app.use('/api/admin', adminRoutes);    
 
 export { app };
